3df448003a28a14d503a869b9d1085aec01fb674
Reyyyss <hcr2207@gmail.com>
2024-11-18 17:25:51 +0000

    vamos la

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: hcarrasq <hcarrasq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 15:40:15 by hcarrasq          #+#    #+#             */
-/*   Updated: 2024/11/18 15:40:47 by hcarrasq         ###   ########.fr       */
+/*   Updated: 2024/11/18 17:24:18 by hcarrasq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 size_t	strlcat(char *dst, const char *src, size_t size)
 {
-	
+	size_t destl;
+	size_t sorcl;
+	size_t i;
+
+	i = 0;
+	destl = ft_strlen(dst);
+	sorcl = ft_strlen(src);
+	if (size <= destl)
+		return size + sorcl;
+	while (destl + i + 1 < size && src[i] != '\0')
+	{
+		dst[destl + i] = src[i];
+		i++;
+	}
+	dst[destl + i] = '\0';
+
+	return (destl + sorcl);
 }
